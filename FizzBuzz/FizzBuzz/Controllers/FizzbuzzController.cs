1380a409a04106739dc310c1c17fc311b53e7f32
berbawy <mohamed.elberbawy@gmail.com>
2018-02-01 00:50:50 +0200

    TDD Initial Fizzbuzz Sample

---
+++ FizzBuzz/FizzBuzz/Controllers/FizzbuzzController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FizzBuzz.Controllers
+{
+    public class FizzbuzzController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetFizzBuzz(string txtValue)
+        {
+            try
+            {
+                string Result = GetFizzBuzzResult(txtValue);
+                return Json(new { results = Result });
+            }
+            catch(Exception ex)
+            {
+                return Json(new { results = ex.Message });
+            }
+        }
+
+       
+
+        public string GetFizzBuzzResult(string val)
+        {
+            if (IsNumber(val))
+            {
+                int number = int.Parse(val);
+                string Result = "";
+                for (int i = 1; i <= number; i++)
+                {
+                    var printNumber = string.Empty;
+                    if (IsFIZZ(i)) printNumber += "Fizz";
+                    if (IsBUZZ(i)) printNumber += "Buzz";
+                    if (IsprintNumber(printNumber))
+                        printNumber = (i).ToString();
+                    Result += " " + printNumber;
+                }
+                return Result.Trim();
+            }
+            else
+                return "Please enter valid number";
+        }
+        public bool IsFIZZ(int v)
+        {
+            return v % 3 == 0;
+        }
+        public bool IsBUZZ(int v)
+        {
+            return v % 5 == 0;
+        }
+
+        public bool IsNumber(string value)
+        {
+            int n;
+            return int.TryParse(value, out n);
+        }
+        public bool IsprintNumber(string printNumber)
+        {
+            return String.IsNullOrEmpty(printNumber);
+        }
+    }
+}
---
+++ FizzBuzz/UnitTestfizzbuzz/FizzBuzzTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz.Controllers;
+
+namespace UnitTestfizzbuzz
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+       
+
+      
+        [TestMethod]
+        
+        public void IsFizzTest()
+        {
+            FizzbuzzController FBC = new FizzbuzzController();
+            int Value = 3;
+            bool IsFizz = FBC.IsFIZZ(Value);
+            Assert.IsTrue(IsFizz);
+        }
+
+        [TestMethod]
+        public void IsBuzzTest()
+        {
+            FizzbuzzController FBC = new FizzbuzzController();
+            int Value = 5;
+            bool IsBuzz= FBC.IsBUZZ(Value);
+            Assert.IsTrue(IsBuzz);
+        }
+        [TestMethod]
+        public void IsNumberTest()
+        {
+            FizzbuzzController FBC = new FizzbuzzController();
+            string txt = "123";
+            bool isNumeric = FBC.IsNumber(txt);
+            Assert.IsTrue(isNumeric);
+
+        }
+
+        [TestMethod]
+        public void TestValue5()
+        {
+            FizzbuzzController FBC = new FizzbuzzController();
+            string txt = "5";
+            string Result = FBC.GetFizzBuzzResult(txt);
+            string ExpR = "1 2 Fizz 4 Buzz";
+            Assert.AreEqual(ExpR, Result);
+
+        }
+
+    }
+}
+
